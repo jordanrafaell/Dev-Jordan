06aa4118d05cf6e70466bfdff32a5f4ff02ba17e
jordanrafaell <jordanrafaelflorencio@gmail.com>
2024-07-10 11:43:27 -0300

    update: button cv/delete: pasta serve

--- scripts/app.js
+++ scripts/app.js
@@ -7,53 +7,60 @@ const rodape = document.querySelector('footer');
 
 // Adiciona a classe active ao botão de modo light
 btnDark.addEventListener('click', () => {
- $html.classList.toggle('dark_mode');
- // Verifica se a classe dark_mode está presente no HTML
- if ($html.classList.contains('dark_mode')) {
-  // Se estiver presente, remove a classe active do botão de modo light
-  btn_light_mode.classList.remove('active');
-  // Adiciona a classe active ao botão de modo dark
-  btn_dark_mode.classList.add('active');
- } else {
-  // Se não estiver presente, remove a classe active do botão de modo dark
-  btn_dark_mode.classList.remove('active');
-  // Adiciona a classe active ao botão de modo light
-  btn_light_mode.classList.add('active');
- }
+  $html.classList.toggle('dark_mode');
+  // Verifica se a classe dark_mode está presente no HTML
+  if ($html.classList.contains('dark_mode')) {
+    // Se estiver presente, remove a classe active do botão de modo light
+    btn_light_mode.classList.remove('active');
+    // Adiciona a classe active ao botão de modo dark
+    btn_dark_mode.classList.add('active');
+  } else {
+    // Se não estiver presente, remove a classe active do botão de modo dark
+    btn_dark_mode.classList.remove('active');
+    // Adiciona a classe active ao botão de modo light
+    btn_light_mode.classList.add('active');
+  }
 });
 
 // Botão menu
 const btnMenu = document.querySelector('#btn_hamburguer');
 const menuNav = document.querySelector('#menu_nav');
+const navList = document.querySelectorAll('#container_list li');
 
 btnMenu.addEventListener('click', () => {
- menuNav.classList.toggle('active_menu');
+  menuNav.classList.toggle('active_menu');
+});
+
+navList.forEach((links) => {
+  links.addEventListener('click', () => {
+    menuNav.classList.remove('active_menu');
+  });
 });
 
 // sumir conteudo
 btnMenu.addEventListener('click', () => {
- if (principal.style.display === 'none') {
-  principal.style.display = 'block';
-  rodape.style.display = 'block';
- } else {
-  principal.style.display = 'none';
-  rodape.style.display = 'none';
- }
+  if (principal.style.display === 'none') {
+    principal.style.display = 'block';
+    rodape.style.display = 'block';
+  } else {
+    principal.style.display = 'none';
+    rodape.style.display = 'none';
+  }
 });
 
 // mudança de imagem light e dark
 // Função para alternar entre os modos light e dark
 function alternarModo() {
- const imagensLight = document.querySelectorAll('.imagem-light');
- const imagensDark = document.querySelectorAll('.imagem-dark');
+  const imagensLight = document.querySelectorAll('.imagem-light');
+  const imagensDark = document.querySelectorAll('.imagem-dark');
 
- if (document.documentElement.classList.contains('dark_mode')) {
-  imagensLight.forEach((img) => (img.style.display = 'block'));
-  imagensDark.forEach((img) => (img.style.display = 'none'));
- } else {
-  imagensLight.forEach((img) => (img.style.display = 'none'));
-  imagensDark.forEach((img) => (img.style.display = 'block'));
- }
+  if (document.documentElement.classList.contains('dark_mode')) {
+    imagensLight.forEach((img) => (img.style.display = 'block'));
+    imagensDark.forEach((img) => (img.style.display = 'none'));
+  } else {
+    imagensLight.forEach((img) => (img.style.display = 'none'));
+    imagensDark.forEach((img) => (img.style.display = 'block'));
+  }
 }
 
 // Event listener para o botão de alternância de modo
@@ -63,38 +70,36 @@ alternarModo();
 
 // Função para navegação de lista
 document.addEventListener('DOMContentLoaded', function () {
- // Oculta todos os projetos, exceto aqueles com o ID "templates"
- var projetos = document.querySelectorAll('.column_projeto');
- projetos.forEach(function (projeto) {
-  if (projeto.id !== 'templates') {
-   projeto.style.display = 'none';
-  }
- });
-
- // Seleciona todos os links da lista
- var links = document.querySelectorAll('.nav_List_projetos a');
-
- // Adiciona um evento de clique a cada link
- links.forEach(function (link) {
-  link.addEventListener('click', function (event) {
-   // Previne o comportamento padrão do link (navegação)
-   event.preventDefault();
-
-   // Obtém o ID do alvo do link
-   var targetId = link.getAttribute('href').substring(1);
-
-   // Oculta todos os projetos
-   projetos.forEach(function (projeto) {
-    projeto.style.display = 'none';
-   });
-
-   // Mostra todos os projetos correspondentes ao ID do link clicado
-   var projetosAlvo = document.querySelectorAll(
-    ".column_projeto[id='" + targetId + "']"
-   );
-   projetosAlvo.forEach(function (projeto) {
-    projeto.style.display = 'flex';
-   });
+  // Oculta todos os projetos, exceto aqueles com o ID "templates"
+  var projetos = document.querySelectorAll('.column_projeto');
+  projetos.forEach(function (projeto) {
+    if (projeto.id !== 'templates') {
+      projeto.style.display = 'none';
+    }
   });
- });
+
+  // Seleciona todos os links da lista
+  var links = document.querySelectorAll('.nav_List_projetos a');
+
+  // Adiciona um evento de clique a cada link
+  links.forEach(function (link) {
+    link.addEventListener('click', function (event) {
+      // Previne o comportamento padrão do link (navegação)
+      event.preventDefault();
+
+      // Obtém o ID do alvo do link
+      var targetId = link.getAttribute('href').substring(1);
+
+      // Oculta todos os projetos
+      projetos.forEach(function (projeto) {
+        projeto.style.display = 'none';
+      });
+
+      // Mostra todos os projetos correspondentes ao ID do link clicado
+      var projetosAlvo = document.querySelectorAll(".column_projeto[id='" + targetId + "']");
+      projetosAlvo.forEach(function (projeto) {
+        projeto.style.display = 'flex';
+      });
+    });
+  });
 });
